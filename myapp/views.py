8f6677932e7053bd4d06d0bc7452501a92948ac1
Cristian Alfredo Manzo Parra <ca.manzo973@uniandes.edu.co>
2022-11-07 18:11:23 -0500

    FEAT: Create routes my app include from django.urls

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
 # Create your views here.
 def hello (request):
-    return HttpResponse("Hello World!")
+    return HttpResponse('''
+    <h1>Hello World!</h1>
+    <p>My first Django app</p>
+    ''')
+def about (request):
+    return HttpResponse('''
+    <h1>About</h1>
+    <p>My My about in Django app</p>
+    ''')
