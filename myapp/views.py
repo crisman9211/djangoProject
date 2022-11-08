03152bd0d6e421e003b049eaccb046ccc4524327
Cristian Alfredo Manzo Parra <ca.manzo973@uniandes.edu.co>
2022-11-08 04:56:31 -0500

    FEAT: render models json render json by url param

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,32 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
+from django.shortcuts import get_object_or_404
+from .models import Project,Task
 # Create your views here.
-def hello (request):
+def index(request):
     return HttpResponse('''
-    <h1>Hello World!</h1>
-    <p>My first Django app</p>
+    Index page
     ''')
+def hello (request,username: str):
+    print(username)
+    return HttpResponse(
+    # '''
+    # <h1>Hello World!</h1>
+    # <p>My first Django app</p>
+    # <p>My name is {username}</p>
+    # '''.format(username=username)
+    "<h1> Hello %s </h1>" % username
+    )
 def about (request):
     return HttpResponse('''
     <h1>About</h1>
     <p>My My about in Django app</p>
     ''')
+def projects (request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects,safe=False)
+    
+def tasks (request,id:int):
+    # tasks = list(Task.objects.filter(id=id).values())
+    # task=Task.objects.get(id=id)
+    task = get_object_or_404(Task,id=id)
+    return HttpResponse('Task: %s' % task.title)
